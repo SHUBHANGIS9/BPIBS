e4cdc7d17c8c85505528ed7ff02edbdd57a7b4b2
SHUBHANGIS9 <shubhangisharma2001@gmail.com>
2025-03-19 17:26:15 +0530

    Update Kira1.py

--- Kira1.py
+++ Kira1.py
@@ -265,6 +265,8 @@ class ArticleApp(MDApp):
         """Reads the title and summary using Azure's Text-to-Speech."""
         title_text = self.title_label.text.replace('Title: ', '').strip()
         summary_text = self.summary_label.text.replace('Summary: ', '').strip()
+        sentiment_text = self.sentiment_label.text.replace('Sentiment: ', '').strip()
+        credibility_text = self.credibility_label.text.replace('Credibility: ', '').strip()
 
         if not summary_text or not title_text:
             print("No title or summary available to read.")
@@ -273,6 +275,7 @@ class ArticleApp(MDApp):
         try:
             # Construct the speech text
             speech_text = f"The summary for the news title {title_text} is: {summary_text}"
+            f"The sentiment is {sentiment_text} and the credibility is {credibility_text}."
 
             # Configure Azure Speech
             speech_config = speechsdk.SpeechConfig(
